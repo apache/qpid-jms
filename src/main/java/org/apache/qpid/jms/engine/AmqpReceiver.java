4f531141b4fcfbe1a9323a75b931b4ac39334964
Robert Gemmell <robbie@apache.org>
2014-08-28 16:34:09 +0000

    QPIDJMS-26: update message receiver to use delivery events to provoke initial processing
    git-svn-id: https://svn.apache.org/repos/asf/qpid/jms/trunk@1621161 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/qpid/jms/engine/AmqpReceiver.java
+++ src/main/java/org/apache/qpid/jms/engine/AmqpReceiver.java
@@ -20,6 +20,9 @@
  */
 package org.apache.qpid.jms.engine;
 
+import java.util.Deque;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
 import org.apache.qpid.proton.engine.Delivery;
 import org.apache.qpid.proton.engine.Receiver;
 import org.apache.qpid.proton.message.Message;
@@ -28,6 +31,9 @@ public class AmqpReceiver extends AmqpLink
 {
     private Receiver _protonReceiver;
     private byte[] _buffer = new byte[1024];
+
+    //TODO: custom queue with timeout based retrieval
+    Deque<AmqpMessage> _messages = new ConcurrentLinkedDeque<AmqpMessage>();
 
     public AmqpReceiver(AmqpSession amqpSession, Receiver protonReceiver, AmqpConnection amqpConnection)
     {
@@ -45,55 +51,58 @@ public class AmqpReceiver extends AmqpLink
 
     public AmqpMessage receiveNoWait()
     {
-        synchronized (getAmqpConnection())
-        {
-            Delivery currentDelivery = _protonReceiver.current();
-            if(currentDelivery != null)
-            {
-                if(currentDelivery.getContext() == null)
-                {
-                    if (currentDelivery.isReadable() && !currentDelivery.isPartial())
-                    {
-
-                        int total = 0;
-                        int start = 0;
-                        while (true)
-                        {
-                            int read = _protonReceiver.recv(_buffer, start, _buffer.length - start);
-                            total += read;
-                            if (read == (_buffer.length - start))
-                            {
-                                //may need to expand the buffer (is there a better test?)
-                                byte[] old = _buffer;
-                                _buffer = new byte[_buffer.length*2];
-                                System.arraycopy(old, 0, _buffer, 0, old.length);
-                                start += read;
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-
-                        Message message = Message.Factory.create();
-                        message.decode(_buffer, 0, total);
-
-                        //TODO: dont create a new factory for every message
-                        AmqpMessage amqpMessage = new AmqpMessageFactory().createAmqpMessage(currentDelivery, message, getAmqpConnection());
-                        currentDelivery.setContext(amqpMessage);
-                        _protonReceiver.advance();
-                        return amqpMessage;
-                    }
-                }
-            }
-        }
-        return null;
+        return _messages.pollFirst();
     }
 
     @Override
     void processDeliveryUpdate(Delivery delivery)
     {
-        //TODO: implement receiver delivery update event processing
+        //TODO: this is currently processing all messages for the link, should really just do the one given.
+        // We can't call recv if the passed delivery is not the 'current', but cant throw the event away either (could be a before-complete disposition change?)
+        // Doesnt handle settlement yet.
+
+        Delivery currentDelivery = _protonReceiver.current();
+        if(currentDelivery != null)
+        {
+            if(currentDelivery.getContext() == null)
+            {
+                if (currentDelivery.isReadable() && !currentDelivery.isPartial())
+                {
+                    int total = 0;
+                    int start = 0;
+                    while (true)
+                    {
+                        int read = _protonReceiver.recv(_buffer, start, _buffer.length - start);
+                        total += read;
+                        if (read == (_buffer.length - start))
+                        {
+                            //may need to expand the buffer (is there a better test?)
+                            byte[] old = _buffer;
+                            _buffer = new byte[_buffer.length*2];
+                            System.arraycopy(old, 0, _buffer, 0, old.length);
+                            start += read;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    Message message = Message.Factory.create();
+                    message.decode(_buffer, 0, total);
+
+                    //TODO: dont create a new factory for every message
+                    AmqpMessage amqpMessage = new AmqpMessageFactory().createAmqpMessage(currentDelivery, message, getAmqpConnection());
+                    currentDelivery.setContext(amqpMessage);
+                    _protonReceiver.advance();
+                    _messages.add(amqpMessage);
+                }
+            }
+            else
+            {
+                //TODO: previously processed this message. Updated disposition info?
+            }
+        }
     }
 
     @Override

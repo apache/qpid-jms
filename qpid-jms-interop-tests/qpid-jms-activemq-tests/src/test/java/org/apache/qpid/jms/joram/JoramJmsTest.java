e22ae37e23fb9c41578c1a7181cd01db8f5e0277
Robert Gemmell <robbie@apache.org>
2015-01-08 11:18:31 +0000

    enable remaining Joram JMS tests

--- qpid-jms-interop-tests/qpid-jms-activemq-tests/src/test/java/org/apache/qpid/jms/joram/JoramJmsTest.java
+++ qpid-jms-interop-tests/qpid-jms-activemq-tests/src/test/java/org/apache/qpid/jms/joram/JoramJmsTest.java
@@ -24,21 +24,25 @@ import org.objectweb.jtests.jms.conform.connection.ConnectionTest;
 import org.objectweb.jtests.jms.conform.connection.TopicConnectionTest;
 import org.objectweb.jtests.jms.conform.message.MessageBodyTest;
 import org.objectweb.jtests.jms.conform.message.MessageDefaultTest;
+import org.objectweb.jtests.jms.conform.message.MessageTypeTest;
 import org.objectweb.jtests.jms.conform.message.headers.MessageHeaderTest;
+import org.objectweb.jtests.jms.conform.message.properties.JMSXPropertyTest;
 import org.objectweb.jtests.jms.conform.message.properties.MessagePropertyConversionTest;
 import org.objectweb.jtests.jms.conform.message.properties.MessagePropertyTest;
+import org.objectweb.jtests.jms.conform.queue.QueueBrowserTest;
 import org.objectweb.jtests.jms.conform.queue.TemporaryQueueTest;
 import org.objectweb.jtests.jms.conform.selector.SelectorSyntaxTest;
 import org.objectweb.jtests.jms.conform.selector.SelectorTest;
 import org.objectweb.jtests.jms.conform.session.QueueSessionTest;
+import org.objectweb.jtests.jms.conform.session.SessionTest;
+import org.objectweb.jtests.jms.conform.session.TopicSessionTest;
+import org.objectweb.jtests.jms.conform.session.UnifiedSessionTest;
 import org.objectweb.jtests.jms.conform.topic.TemporaryTopicTest;
 
 public class JoramJmsTest extends TestCase {
 
     public static Test suite() {
         TestSuite suite = new TestSuite();
-
-        // TODO: Fix failing tests..
 
         // Connection
         suite.addTestSuite(ConnectionTest.class);
@@ -47,18 +51,18 @@ public class JoramJmsTest extends TestCase {
         //Message
         suite.addTestSuite(MessageBodyTest.class);
         suite.addTestSuite(MessageDefaultTest.class);
-        // suite.addTestSuite(MessageTypeTest.class);
+        suite.addTestSuite(MessageTypeTest.class);
 
         //Message Headers
         suite.addTestSuite(MessageHeaderTest.class);
 
         //Message Properties
-        // suite.addTestSuite(JMSXPropertyTest.class);
+        suite.addTestSuite(JMSXPropertyTest.class);
         suite.addTestSuite(MessagePropertyConversionTest.class);
         suite.addTestSuite(MessagePropertyTest.class);
 
         //Queue
-        // suite.addTestSuite(QueueBrowserTest.class);
+        suite.addTestSuite(QueueBrowserTest.class);
         suite.addTestSuite(TemporaryQueueTest.class);
 
         //Selector
@@ -67,10 +71,9 @@ public class JoramJmsTest extends TestCase {
 
         //Session
         suite.addTestSuite(QueueSessionTest.class);
-        // suite.addTestSuite(SessionTest.class);
-        // Next test fails due to: https://issues.apache.org/jira/browse/PROTON-154
-        // suite.addTestSuite(TopicSessionTest.class);
-        // suite.addTestSuite(UnifiedSessionTest.class);
+        suite.addTestSuite(SessionTest.class);
+        suite.addTestSuite(TopicSessionTest.class);
+        suite.addTestSuite(UnifiedSessionTest.class);
 
         //Topic
         suite.addTestSuite(TemporaryTopicTest.class);

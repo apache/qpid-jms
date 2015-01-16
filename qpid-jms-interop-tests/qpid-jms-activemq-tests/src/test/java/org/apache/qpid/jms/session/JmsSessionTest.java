a52fc583616bdaa9ad63d7c4b4e5e0f772232669
Robert Gemmell <robbie@apache.org>
2015-01-16 17:34:18 +0000

    move remaining session test to the session package

--- qpid-jms-interop-tests/qpid-jms-activemq-tests/src/test/java/org/apache/qpid/jms/session/JmsSessionTest.java
+++ qpid-jms-interop-tests/qpid-jms-activemq-tests/src/test/java/org/apache/qpid/jms/session/JmsSessionTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.qpid.jms;
+package org.apache.qpid.jms.session;
 
 import static org.junit.Assert.assertNotNull;
 

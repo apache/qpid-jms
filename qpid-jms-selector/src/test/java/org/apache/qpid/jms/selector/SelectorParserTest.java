5f147662b32e6a545eae6cfaef8b16c7b0cb617b
Robert Gemmell <robbie@apache.org>
2015-02-17 17:49:51 +0000

    rename test package to match main

--- qpid-jms-selector/src/test/java/org/apache/qpid/jms/selector/SelectorParserTest.java
+++ qpid-jms-selector/src/test/java/org/apache/qpid/jms/selector/SelectorParserTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.activemq.apollo.selector;
+package org.apache.qpid.jms.selector;
 
 import junit.framework.TestCase;
 

6d03872afb1b44408bdb52907b8a88ce2da64894
Robert Gemmell <robbie@apache.org>
2015-02-20 13:52:22 +0000

    add test of session+consumer behaviour on connection which is remotely ended

--- qpid-jms-client/src/test/java/org/apache/qpid/jms/integration/ConnectionIntegrationTest.java
+++ qpid-jms-client/src/test/java/org/apache/qpid/jms/integration/ConnectionIntegrationTest.java
@@ -23,22 +23,27 @@ package org.apache.qpid.jms.integration;
 import static org.hamcrest.Matchers.arrayContaining;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionMetaData;
+import javax.jms.IllegalStateException;
+import javax.jms.MessageConsumer;
+import javax.jms.Queue;
 import javax.jms.Session;
 
 import org.apache.qpid.jms.test.QpidJmsTestCase;
 import org.apache.qpid.jms.test.testpeer.TestAmqpPeer;
 import org.apache.qpid.jms.test.testpeer.matchers.CoordinatorMatcher;
 import org.apache.qpid.proton.amqp.transaction.TxnCapability;
+import org.junit.Ignore;
 import org.junit.Test;
 
 // TODO find a way to make the test abort immediately if the TestAmqpPeer throws an exception
 public class ConnectionIntegrationTest extends QpidJmsTestCase {
     private final IntegrationTestFixture testFixture = new IntegrationTestFixture();
 
-    @Test(timeout=10000)
+    @Test(timeout = 5000)
     public void testCreateAndCloseConnection() throws Exception {
         try (TestAmqpPeer testPeer = new TestAmqpPeer();) {
             Connection connection = testFixture.establishConnecton(testPeer);
@@ -47,7 +52,7 @@ public class ConnectionIntegrationTest extends QpidJmsTestCase {
         }
     }
 
-    @Test(timeout=10000)
+    @Test(timeout = 5000)
     public void testCreateAutoAckSession() throws Exception {
         try (TestAmqpPeer testPeer = new TestAmqpPeer();) {
             Connection connection = testFixture.establishConnecton(testPeer);
@@ -57,7 +62,7 @@ public class ConnectionIntegrationTest extends QpidJmsTestCase {
         }
     }
 
-    @Test(timeout=5000)
+    @Test(timeout = 5000)
     public void testCreateTransactedSession() throws Exception {
         try (TestAmqpPeer testPeer = new TestAmqpPeer();) {
             Connection connection = testFixture.establishConnecton(testPeer);
@@ -74,13 +79,55 @@ public class ConnectionIntegrationTest extends QpidJmsTestCase {
         }
     }
 
-    @Test(timeout=5000)
+    @Test(timeout = 5000)
     public void testConnectionMetaDataVersion() throws Exception {
         try (TestAmqpPeer testPeer = new TestAmqpPeer();) {
             Connection connection = testFixture.establishConnecton(testPeer);
             ConnectionMetaData meta = connection.getMetaData();
             int result = meta.getProviderMajorVersion() + meta.getProviderMinorVersion();
             assertTrue("Expected non-zero provider major / minor version", result != 0);
+        }
+    }
+
+    @Ignore // TODO: resolve related issues and enable
+    @Test(timeout = 5000)
+    public void testRemotelyEndConnectionWithSessionWithConsumer() throws Exception {
+        try (TestAmqpPeer testPeer = new TestAmqpPeer();) {
+            Connection connection = testFixture.establishConnecton(testPeer);
+
+            testPeer.expectBegin(true);
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create a consumer, then remotely end the connection afterwards.
+            testPeer.expectReceiverAttach();
+            testPeer.expectLinkFlow();
+            testPeer.remotelyEndConnection(true);
+
+            Queue queue = session.createQueue("myQueue");
+            MessageConsumer consumer = session.createConsumer(queue);
+
+            testPeer.waitForAllHandlersToComplete(1000);
+
+            // Verify the session is now marked closed
+            try {
+                session.getAcknowledgeMode();
+                fail("Expected ISE to be thrown due to being closed");
+            } catch (IllegalStateException jmsise) {
+                // expected
+            }
+
+            // Verify the consumer is now marked closed
+            try {
+                consumer.getMessageListener();
+                fail("Expected ISE to be thrown due to being closed");
+            } catch (IllegalStateException jmsise) {
+                // expected
+            }
+
+            // Try closing them explicitly, should effectively no-op in client.
+            // The test peer will throw during close if it sends anything.
+            consumer.close();
+            session.close();
         }
     }
 }
